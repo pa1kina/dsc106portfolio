5d5613e67594caa4f888b02b577b3f843486027c
pa1kina <p.volskaya@gmail.com>
2026-04-17 20:10:22 -0700

    js functionality!

---
+++ global.js
@@ -0,0 +1,71 @@
+console.log("IT’S ALIVE!");
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// navLinks = $$("nav a")
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/" // Local server
+    : "/website/"; // GitHub Pages repo name
+
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "resume/", title: "Resume" },
+  { url: "contact/", title: "Contact" },
+  { url: "https://github.com/pa1kina", title: "Github" },
+];
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  url = !url.startsWith("http") ? BASE_PATH + url : url;
+  let title = p.title;
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+}
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+			<option value='light dark'>Automatic</option>
+      <option value='light'>Light</option>
+      <option value='dark'>Dark</option>
+		</select>
+	</label>`,
+);
+
+const select = document.querySelector(".color-scheme select");
+if ("colorScheme" in localStorage) {
+  document.documentElement.style.setProperty("color-scheme", localStorage.colorScheme);
+  select.value = localStorage.colorScheme;
+}
+
+select.addEventListener("input", function (event) {
+  document.documentElement.style.setProperty(
+    "color-scheme",
+    event.target.value,
+  );
+  localStorage.colorScheme = event.target.value;
+  console.log("color scheme changed to", event.target.value);
+});
